7919265f5a12a0fb9107471b01482afb83421771
Mateus Carvalho <mateus.raimundo1995@gmail.com>
2024-01-31 23:34:32 -0300

    updated project

--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -4,6 +4,7 @@ import styles from './ReposList.module.css';
 const ReposList = ({ nomeUsuario }) => {
     const [repos, setRepos] = useState([]);
     const [estaCarregando, setEstaCarregando] = useState(false);
+    console.log(repos);
 
     useEffect(() => {
         setEstaCarregando(true);
@@ -19,28 +20,36 @@ const ReposList = ({ nomeUsuario }) => {
 
     return (
         <div className="container">
-            {
-                estaCarregando ? (
-                    <h1>Carregando...</h1>
-                ) : (
-                    <ul className={styles.list}>
-                        {/*repos.map(item => (*/}
-                        {
-                            repos.map(({ id, name, language, html_url }) => (
-                                <li key={id} className={styles.listItem}>
-                                    <div className={styles.itemName}>
-                                        <b>Nome:</b> {name} <br />
-                                    </div>
-                                    <div className={styles.itemLanguage}>
-                                        <b>Linguagem:</b> {language} <br />
-                                    </div>
-                                    <a className={styles.itemLink} target="_blank" href={html_url}>Visitar no Github</a>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                )
-            }
+            {estaCarregando ? (
+                <h1>Carregando...</h1>
+            ) : (
+                <>
+                    {
+                        repos?.message === 'Not Found' ? (
+                            <div className="container">
+                                <h1>Usuário não existe!</h1>
+                            </div>
+                        ) : (
+                            <ul className={styles.list}>
+                                {/*repos.map(item => (*/}
+                                {
+                                    repos.map(({ id, name, language, html_url }) => (
+                                        <li key={id} className={styles.listItem}>
+                                            <div className={styles.itemName}>
+                                                <b>Nome:</b> {name} <br />
+                                            </div>
+                                            <div className={styles.itemLanguage}>
+                                                <b>Linguagem:</b> {language} <br />
+                                            </div>
+                                            <a className={styles.itemLink} target="_blank" href={html_url}>Visitar no Github</a>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        )
+                    }
+                </>
+            )}
         </div>
     )
 }
